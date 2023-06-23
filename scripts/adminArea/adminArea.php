6473d0354abf35a86fc11a5a7355fafa4d549d8c
Angel Velasco <angelgg2020@outlook.com>
2023-06-23 07:27:59 -0500

    feat: :sparkles: Add CRUD to admin_area table

---
+++ scripts/adminArea/adminArea.php
@@ -0,0 +1,93 @@
+<?
+class adminArea extends connect
+{
+    private $queryPost = 'INSERT INTO admin_area(id, id_area, id_staff, id_position, id_journey) VALUES (:identification, :areaId, :staffId, :positionId, journeyId)';
+    private $queryGet = 'SELECT id AS "identification", SELECT id_area AS "areaId", SELECT id_staff AS "staffId", SELECT id_position AS "positionId", SELECT id_journey AS "journeryId FROM admin_area';
+    private $queryUpdate = 'UPDATE admin_area SET id_area = :areaId, id_staff = :staffId, id_position = :positionId, id_journey = :journeyId WHERE id = :identification';
+    private $queryDelete = 'DELETE FROM admin_area WHERE id = :identification';
+    private $msg;
+    use Singleton;
+
+    //? Constructor */
+    function __construct(private $id = 1, private $id_area = 1, private $id_staff = 1, private $id_position, private $id_journey = 1)
+    {
+        parent::__construct();
+    }
+
+    //? POST Function */
+    public function adminAreaPost()
+    {
+        try {
+            $sentence = $this->conx->prepare($this->queryPost);
+
+            $sentence->bindValue("identification", $this->id);
+            $sentence->bindValue("areaId", $this->id_area);
+            $sentence->bindValue("staffId", $this->id_staff);
+            $sentence->bindValue("positionId", $this->id_position);
+            $sentence->bindValue("journeyId", $this->id_journey);
+
+            $sentence->execute();
+
+            $this->msg = ["Code" => 200 + $sentence->rowCount(), "Message" => "Inserted Data"];
+        } catch (\PDOException $e) {
+            $this->msg = ["Code" => $e->getCode(), "Message" => $sentence->errorInfo()[2]];
+        } finally {
+            print_r($this->msg);
+        }
+    }
+    //? GET Function */
+    public function adminAreaGet()
+    {
+        try {
+            $sentence = $this->conx->prepare($this->queryGet);
+            $sentence->execute();
+
+            $this->msg = ["Code" => 200, "Message" => $sentence->fetchAll(PDO::FETCH_ASSOC)];
+
+        } catch (\PDOException $e) {
+            $this->msg = ["Code" => $e->getCode(), "Message" => $sentence->errorInfo()[2]];
+        } finally {
+            print_r($this->msg);
+        }
+    }
+
+    //? UPDATE Function */
+    function adminAreaUpdate()
+    {
+        try {
+            $sentence = $this->conx->prepare($this->queryUpdate);
+
+            $sentence->bindValue("identification", $this->id);
+            $sentence->bindValue("areaId", $this->id_area);
+            $sentence->bindValue("staffId", $this->id_staff);
+            $sentence->bindValue("positionId", $this->id_position);
+            $sentence->bindValue("journeysId", $this->id_journey);
+            $sentence->execute();
+
+            ($sentence->rowCount() > 0) ? $this->msg = ["Code" => 200, "Message" => "Updated Data"] : "none";
+        } catch (\PDOException $e) {
+            $this->msg = ["Code" => $e->getCode(), "Message" => $sentence->errorInfo()[2]];
+        } finally {
+            print_r($this->msg);
+        }
+    }
+    //? DELETE Function */
+    function adminAreaDelete()
+    {
+        try {
+            $sentence = $this->conx->prepare($this->queryDelete);
+            $sentence->bindValue("identification", $this->id);
+            $sentence->execute();
+
+            $this->msg = ["Code" => 200, "Message" => "Deleted Data"];
+        } catch (\PDOException $e) {
+            $this->msg = ["Code" => $e->getCode(), "Message" => $sentence->errorInfo()[2]];
+        } finally {
+            print_r($this->msg);
+        }
+    }
+
+
+
+}
+?>
