1c6d0d85ff78198b1431197f6ab484a835be9e84
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-06-24 12:35:34 -0500

    feat: :sparkles: Added CRUD to maint_area table

--- uploads/app.php
+++ uploads/app.php
@@ -4,8 +4,8 @@
     require_once "../vendor/autoload.php";
     $router = new \Bramus\Router\Router();
 
-    $router->post('/design', function() {
-        App\design_area::Singleton(json_decode(file_get_contents("php://input")))->designAreaPost();
+    $router->post('/maint', function() {
+        App\maint_area::Singleton(json_decode(file_get_contents("php://input"), true))->maintAreaPost();
     });
     $router->run();
     
