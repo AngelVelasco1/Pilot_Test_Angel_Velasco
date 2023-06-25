011385365d98696ec80e50771fd835fb7ae3cf79
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-06-25 14:52:07 -0500

    feat: :sparkles: Added CRUD to team_educators table

---
+++ scripts/team_educators/team_educators.php
@@ -0,0 +1,92 @@
+<?php
+namespace App\team_educators;
+use App\db\connect;
+use App\Singleton;
+class team_educators extends connect
+{
+    private $queryPost = 'INSERT INTO team_educators(id, name_rol) VALUES (:identification, :rol, :id_country)';
+    private $queryGet = 'SELECT id AS "identification", SELECT name_rol AS "rol", SELECT id_country AS "id_country" FROM team_educators';
+    private $queryUpdate = 'UPDATE team_educators SET name_rol = :rol WHERE id = :identification';
+    private $queryDelete = 'DELETE FROM team_educators WHERE id = :identification';
+    private $msg;
+
+    use Singleton;
+
+    //? Constructor */
+    function __construct(private $id = 1, private $name_rol= 1)
+    {
+        parent::__construct();
+    }
+
+    //? POST Function */
+    public function team_educatorsPost()
+    {
+        try {
+            $sentence = $this->conx->prepare($this->queryPost);
+
+            $sentence->bindValue("identification", $this->id);
+            $sentence->bindValue("rol", $this->name_rol);
+
+            $sentence->execute();
+
+            $this->msg = ["Code" => 200 + $sentence->rowCount(), "Message" => "Inserted Data"];
+        } catch (\PDOException $e) {
+            $this->msg = ["Code" => $e->getCode(), "Message" => $sentence->errorInfo()[2]];
+        } finally {
+            print_r($this->msg);
+        }
+    }
+    //? GET Function */
+    public function team_educatorsGet()
+    {
+        try {
+            $sentence = $this->conx->prepare($this->queryGet);
+            $sentence->execute();
+
+            $this->msg = ["Code" => 200, "Message" => $sentence->fetchAll(\PDO::FETCH_ASSOC)];
+
+        } catch (\PDOException $e) {
+            $this->msg = ["Code" => $e->getCode(), "Message" => $sentence->errorInfo()[2]];
+        } finally {
+            print_r($this->msg);
+        }
+    }
+
+    //? UPDATE Function */
+    function team_educatorsUpdate()
+    {
+        try {
+            $sentence = $this->conx->prepare($this->queryUpdate);
+
+            $sentence->bindValue("identification", $this->id);
+            $sentence->bindValue("rol", $this->name_rol);
+  
+            $sentence->execute();
+
+            ($sentence->rowCount() > 0) ? $this->msg = ["Code" => 200, "Message" => "Updated Data"] : "none";
+        } catch (\PDOException $e) {
+            $this->msg = ["Code" => $e->getCode(), "Message" => $sentence->errorInfo()[2]];
+        } finally {
+            print_r($this->msg);
+        }
+    }
+    //? DELETE Function */
+    function team_educatorsDelete()
+    {
+        try {
+            $sentence = $this->conx->prepare($this->queryDelete);
+            $sentence->bindValue("identification", $this->id);
+            $sentence->execute();
+
+            $this->msg = ["Code" => 200, "Message" => "Deleted Data"];
+        } catch (\PDOException $e) {
+            $this->msg = ["Code" => $e->getCode(), "Message" => $sentence->errorInfo()[2]];
+        } finally {
+            print_r($this->msg);
+        }
+    }
+
+
+
+}
+?>
