96e13b7b95033e3738e0d8c82f2da56a744ca1d1
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-06-25 11:27:44 -0500

    feat: :sparkles: Added CRUD to english_skills table

--- uploads/app.php
+++ uploads/app.php
@@ -4,8 +4,8 @@
     require_once "../vendor/autoload.php";
     $router = new \Bramus\Router\Router();
 
-    $router->post('/software_skills', function() {
-        App\software_skills::Singleton(json_decode(file_get_contents("php://input"), true))->softwareSkillsPost();
+    $router->post('/english_skills', function() {
+        App\english_skills::Singleton(json_decode(file_get_contents("php://input"), true))->englishSkillsPost();
     });
     $router->run();
     
